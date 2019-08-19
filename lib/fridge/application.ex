1b412eb7c3811670b9cf7b5f3955823f385b5dcc
pavel.gavrilov <pavel.gavrilov@binomo.com>
2019-08-19 14:33:58 +0300

    code review

--- lib/fridge/application.ex
+++ lib/fridge/application.ex
@@ -2,20 +2,15 @@ defmodule Fridge.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
+    Supervisor.start_link(children(), strategy: :one_for_one)
+  end
+
+  defp children do
+    [
       {Fridge, name: Fridge},
       {NoHumanFridge, name: NoHumanFridge},
       {VeganFridge, name: VeganFridge}
     ]
-
-    opts = [
-      strategy: :one_for_one,
-      name: Fridge.Supervisor,
-      strategy: :one_for_one,
-      name: NoHumanFridge.Supervisor,
-      strategy: :one_for_one,
-      name: VeganFridge.Supervisor,
-    ]
-    Supervisor.start_link(children, opts)
   end
+
 end
