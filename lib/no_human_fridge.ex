354f7eaa5bf845acce24b3eecd8968b94b1532bf
pavel.gavrilov <pavel.gavrilov@binomo.com>
2019-08-19 11:28:47 +0300

    checkpoint

--- lib/no_human_fridge.ex
+++ lib/no_human_fridge.ex
@@ -6,6 +6,7 @@ defmodule NoHumanFridge do
   @lookup :lookup
   @create :create
   @delete :delete
+  @clean :clean
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts)
@@ -19,17 +20,27 @@ defmodule NoHumanFridge do
     GenServer.cast(server, {@create, name})
   end
 
-  def delete(server, name) do
-    GenServer.cast(server, {@delete, name})
+  def delete(server, food = %{:name => _, expiry_date: _}) do
+    GenServer.cast(server, {@delete, food})
+  end
+
+  defp clean(server) do
+    GenServer.cast(server, @delete)
+    Process.send_after(self, @clean, 1000)
   end
 
   def init(:ok) do
     IO.puts("Started NoHumanFridge: #{inspect self}")
+    Process.send_after(self, @clean, 1000)
     {:ok, %{}}
   end
 
   def handle_call({@lookup, name}, _from, state) do
-    {@reply, Map.fetch(state, name), state}
+    result = case Map.fetch(state, name) do
+      :error -> "#{name} not found"
+      {:ok, expiry_date_list} -> Enum.map(expiry_date_list, fn x -> "Name: #{name}, expiry date: #{x}" end)
+    end
+    {@reply, result, state}
   end
 
   #  %{:type => "human", :name => "Oleg"}
@@ -46,28 +57,43 @@ defmodule NoHumanFridge do
     end
   end
 
-  def handle_cast({@delete, %{:name => name, expiry_date: expiry_date}}, state) do
-    case Map.fetch(state, name) do
-      :error ->
-        {@noreply, state}
-      {:ok, expiry_date_list} ->
-        result = Enum.filter(expiry_date_list, fn date -> NaiveDateTime.compare(date, expiry_date) != :eq end)
-        if Enum.empty?result do
-          {@noreply, Map.delete(state, name)}
+  def handle_cast(@delete, state) do
+    expiry_date = NaiveDateTime.utc_now()
+    state = Enum.reduce(
+      state,
+      %{},
+      fn {name, expiry_dates}, acc ->
+        remain = Enum.filter(
+          expiry_dates,
+          fn date -> case NaiveDateTime.compare(date, expiry_date) do
+                       :gt -> true
+                       _ ->
+                         IO.puts("Dropped: #{name}, expiry date: #{date}")
+                         false
+                     end
+          end
+        )
+        if Enum.empty?(remain) do
+          acc
         else
-          {@noreply, Map.put(state, name, result)}
+          Map.put(acc, name, remain)
         end
-
-    end
+      end
+    )
+    {@noreply, state}
   end
 
-  def handle_info(_msg, state) do
-    IO.puts("ICE")
+  def handle_info(msg, state) do
+    case msg do
+      @clean -> clean(self)
+      _ -> IO.puts("ICE")
+    end
     {@noreply, state}
   end
 
 end
 
+# NaiveDateTime.utc_now()
 # NoHumanFridge.create(NoHumanFridge, %{:type => "fruit", :name => "apple", :expiry_date =>  ~N[2000-01-01 00:00:00]})
 # NoHumanFridge.lookup(NoHumanFridge, "apple")
 # NoHumanFridge.delete(NoHumanFridge, %{:name => "apple", :expiry_date =>  ~N[2000-01-01 00:00:00]})
